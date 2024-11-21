c1056d4c5571190bc6ec6187a8abdda2857e03c1
sarahraquely <160151647+sarahraquely@users.noreply.github.com>
2024-11-21 11:56:36 -0300

    native_telas

---
+++ src/mocks/textos.js
@@ -0,0 +1,23 @@
+const textos = {
+    boasVindas: "Olá Natalia",
+    legenda: "Encontre os produtores mais próximos a você!",
+    legendaMelhoresProdutores: "Veja os produtores com 4+ estrelas!",
+    tituloProdutores: "Produtores",
+    tituloProdutor: "Detalhe do produtor",
+    tituloCestas: "Cestas",
+    topoCesta: "Detalhe da cesta",
+    botaoComprar: "Comprar",
+    tituloItens: "Itens da cesta",
+};
+
+export default textos;
+
+
+    mensagemCompra: "Agradecemos pela compra da cesta $NOME, seu pedido chegará em breve!"
+    mensagemCompra: "Agradecemos pela compra da cesta $NOME, seu pedido chegará em breve!",
+    topoCompra: "Pedido feito com sucesso!",
+    tituloCompra: "Parabéns!",
+    botaoHomeCompra: "Voltar a Home",
+    botaoProdutorCompra: "Voltar ao Produtor",
+
+
